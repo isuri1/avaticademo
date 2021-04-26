8a47efa69a012c35b5472fce245c3d84cd7f4e3c
Isuri1 <isubabe1>
2021-04-26 10:04:58 +0800

    first commit

---
+++ src/main/java/com/zuhlke/avaticademo/AvaticademoApplication.java
@@ -0,0 +1,13 @@
+package com.zuhlke.avaticademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvaticademoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvaticademoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zuhlke/avaticademo/AvaticademoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zuhlke.avaticademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvaticademoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
